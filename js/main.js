ccdab131571aa8afb34588a82e414f2be6346c75
Pachj <Pachj@users.noreply.github.com>
2017-02-20 17:32:22 +0100

    full functionality for Online and Offline Channels added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /**
  * Created by Henry on 20.02.17.
  */
-const channels = ["wintergaming"];
+const channels = ["summonersinnlive", "freecodecamp"];
 
 $(document).ready(() => {
     checkOnline();
@@ -40,10 +40,32 @@ function checkIfExisting(channelNameToCheck) {
 }
 
 
-function displayOnlineChannel(chanelToDisplay) {
+function displayOnlineChannel(channelToDisplay) {
+    let logo = channelToDisplay.stream.channel.logo;
+    let name = channelToDisplay.stream.channel.display_name;
+    let game = channelToDisplay.stream.game;
+    let description = channelToDisplay.stream.channel.status;
 
+/*    $("#logo").attr("src", logo);
+    $("#name").html(name);
+    $("#game").html(game);
+    $("#description").html(description);*/
+
+    $("#online").append('<div><div class="row text-center"><div class="col-xs-2"><img class="logo" src="' + logo + '"></div>' +
+        '<div class="col-xs-10 col-md-3"><h4>' + name + '</h4></div><div class="col-xs-12 col-md-7">' +
+        '<h4>' + game + '</h4></div></div><div class="row"><div class="col-md-12"><p>' + description + '</p></div></div></div>');
 }
 
-function displayOfflineChannel(chanelToDisplay) {
-    
+function displayOfflineChannel(channelToDisplay) {
+    let logo = channelToDisplay.logo;
+    let name = channelToDisplay.display_name;
+    let description = "Offline";
+
+/*    $("#offline-logo").attr("src", logo);
+    $("#offline-name").html(name);
+    $("#offline-description").html(description);*/
+
+    $("#offline").append('<div><div class="row text-center"><div class="col-xs-2"><img class="logo" src="' + logo + '"></div>' +
+        '<div class="col-xs-10 col-md-3"><h4>' + name + '</h4></div></div><div class="row">' +
+        '<div class="col-md-12"><p>' + description + '</p></div></div></div>');
 }
