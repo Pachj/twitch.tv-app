00726f4f42b81d4af32d82fbc2ad544644e30a50
Pachj <Pachj@users.noreply.github.com>
2017-02-20 17:02:38 +0100

    skeleton added

--- js/main.js
+++ js/main.js
@@ -1,36 +1,49 @@
 /**
  * Created by Henry on 20.02.17.
  */
-const streams = ["wintergaming"];
+const channels = ["wintergaming"];
 
 $(document).ready(() => {
-    getStreams();
+    checkOnline();
 });
 
-function getStreams() {
-    for (let i = 0; i < streams.length; i++) {
-        $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + streams[i], (data) => {
-            console.log(data);
-            displayStream(data);
+//Checks if a channel is Online
+function checkOnline() {
+    for (let i = 0; i < channels.length; i++) {
+        $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + channels[i], (stream) => {
+            //If the channel is Online --> Display the Channel
+            if (stream.stream) {
+                displayOnlineChannel(stream);
+            }
+
+            //If the channel not is Online --> Check if the channel is existing
+            else {
+                checkIfExisting(channels[i]);
+            }
         })
     }
 }
 
-function displayStream(data) {          //ToDo: Add check if Online (typeOf?), also display the Game
-    let logo = data.stream.channel.logo;
-    let name = data.stream.channel.display_name;
-    let description = data.stream.channel.status;
+//Checks if a channel is Existing
+function checkIfExisting(channelNameToCheck) {
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + channelNameToCheck, (channel) => {
+        //If the channel not is existing -->
+        if (typeof channel.status === "number") {
 
-    if (description.length > 47) {
-        description = description.slice(0, 47) + "...";
-    }
+        }
 
-    $("#logo").attr("src", logo);
-    $("#name").html(name);
-    $("#description").html(description);
+        //If the channel is existing --> display the channel
+        else {
+            displayOfflineChannel(channel);
+        }
+    })
+}
 
 
-/*    $("#streams").append('<div><div class="row"><div class="col-xs-2">' +
-        '<img class="logo" src="' + logo + '"></div><div class="col-xs-10 col-md-3 name"><p>' + name + '</p></div>' +
-        '<div class="col-xs-10 col-md-7"><p>' + description + '</p></div></div></div>')*/
+function displayOnlineChannel(chanelToDisplay) {
+
+}
+
+function displayOfflineChannel(chanelToDisplay) {
+    
 }
